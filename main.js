1e6f6622f2ab31154cd060e66f9f271e30220e24
AbhipshaCoder <91730739+AbhipshaCoder@users.noreply.github.com>
2022-09-21 18:51:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,21 +16,26 @@ recognition.onresult = function (event) {
 
     document.getElementById("textarea").innerHTML = Content;
     console.log(Content); 
-
+    if(Content == "take my selfie"){
     speak();
+    }
 }
 
 function speak()
 {
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textarea").value;
+    speak_data = "Taking your selfie in 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
 
     Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
 }
 
 camera = document.getElementById("camera");
@@ -40,3 +45,21 @@ Webcam.set({
     image_format : 'png',
     png_quality : 90
 });
+
+function take_snapshot()
+{
+    Webcam.snap(
+        function(data_uri)
+        {
+          document.getElementById("result").innerHTML = '<img id= "selfie_image" src="'+data_uri+'">';
+        }
+    );
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
